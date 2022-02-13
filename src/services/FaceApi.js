c33e6f02c1c02a5208e28c505c5e0a1f4adf9b84
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-02-13 14:25:48 +0530

    Add. logic for api call for ImageUpload

--- src/services/FaceApi.js
+++ src/services/FaceApi.js
@@ -4,16 +4,30 @@ const suscriptionKey = "< AZURE-SUBSCRIPTION-KEY >";
 const faceAttributes = "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
 const detectionModel = "detection_01";
 export const faceApiForUrl = axios.create({
- baseURL: baseURL,
- timeout: 50000,
- headers: {
-  "Ocp-Apim-Subscription-Key": suscriptionKey,
-  "Content-Type": "application/json"
- },
- params: {
-  returnFaceId: true,
-  returnFaceLandmarks: false,
-  returnFaceAttributes: faceAttributes,
-  detectionModel: detectionModel
- }
+    baseURL: baseURL,
+    timeout: 50000,
+    headers: {
+        "Ocp-Apim-Subscription-Key": suscriptionKey,
+        "Content-Type": "application/json"
+    },
+    params: {
+        returnFaceId: true,
+        returnFaceLandmarks: false,
+        returnFaceAttributes: faceAttributes,
+        detectionModel: detectionModel
+    }
+});
+export const faceApiForUpload = axios.create({
+    baseURL: baseURL,
+    timeout: 50000,
+    headers: {
+        "Ocp-Apim-Subscription-Key": suscriptionKey,
+        "Content-Type": "application/octet-stream"
+    },
+    params: {
+        returnFaceId: true,
+        returnFaceLandmarks: false,
+        returnFaceAttributes: faceAttributes,
+        detectionModel: detectionModel
+    }
 });
