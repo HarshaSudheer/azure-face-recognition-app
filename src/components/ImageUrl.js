c768978e18a01eed53c35aee05b66691ef777e02
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-02-13 14:19:47 +0530

    Add. handleSubmit method to call api with image url

--- src/components/ImageUrl.js
+++ src/components/ImageUrl.js
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
+import { faceApiForUrl } from "../services/FaceApi";
 const ImageUrl = () => {
- const [image, setImage] = useState("");
- const [outputImage, setOutputImage] = useState(false);
- const handleSubmit = () => {
-  setOutputImage(true);
- }
- const handleBack = () => {
-  setOutputImage(false);
-  setImage("");
- }
- return (
-  <div>
-   {(!outputImage) ?
-   <div>
-    <input type="text" placeholder="paste image url here" value={image} onChange={event => setImage(event.target.value)} />
-    <button type="button" onClick={handleSubmit}>SUBMIT</button>
-   </div>
-   :
-   <div>
-    <img src={image} alt="output" /><br/>
-    <button type="button" onClick={handleBack}>BACK</button>
-   </div>
-   }
-  </div>
- );
+    const [data, setData] = useState([])
+    const [image, setImage] = useState("");
+    const [outputImage, setOutputImage] = useState(false);
+    const handleSubmit = async () => {
+        try {
+            const response = await faceApiForUrl.post(
+                `/face/v1.0/detect`,
+                {
+                    url: image
+                }
+            );
+            setData(response.data);
+            setOutputImage(true);
+        }
+        catch (err) {
+            console.log(err.response.data);
+            window.alert("An error occurred");
+        }
+    }
+    const handleBack = () => {
+        setOutputImage(false);
+        setImage("");
+    }
+    return (
+        <div>
+            {(!outputImage) ?
+                <div>
+                    <input type="text" placeholder="paste image url here" value={image} onChange={event => setImage(event.target.value)} />
+                    <button type="button" onClick={handleSubmit}>SUBMIT</button>
+                </div>
+                :
+                <div>
+                    <img src={image} alt="output" /><br />
+                    <button type="button" onClick={handleBack}>BACK</button>
+                </div>
+            }
+        </div>
+    );
 }
 export default ImageUrl;
