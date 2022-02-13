476bf144ffb31c47ed0351d3ea96111f9170416b
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-02-13 14:14:38 +0530

    Add. basic elements in ImageUrl

--- src/components/ImageUrl.js
+++ src/components/ImageUrl.js
@@ -1,9 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
 const ImageUrl = () => {
-    return(
-        <div></div>
-    )
+ const [image, setImage] = useState("");
+ const [outputImage, setOutputImage] = useState(false);
+ const handleSubmit = () => {
+  setOutputImage(true);
+ }
+ const handleBack = () => {
+  setOutputImage(false);
+  setImage("");
+ }
+ return (
+  <div>
+   {(!outputImage) ?
+   <div>
+    <input type="text" placeholder="paste image url here" value={image} onChange={event => setImage(event.target.value)} />
+    <button type="button" onClick={handleSubmit}>SUBMIT</button>
+   </div>
+   :
+   <div>
+    <img src={image} alt="output" /><br/>
+    <button type="button" onClick={handleBack}>BACK</button>
+   </div>
+   }
+  </div>
+ );
 }
-
 export default ImageUrl;
