825ac6c911d2a4a9a8243f8b73fc4f989f2bc36a
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-02-14 17:24:28 +0530

    Refactor.

--- src/services/FaceApi.js
+++ src/services/FaceApi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 const baseURL = "https://faceapilearning.cognitiveservices.azure.com";
-const suscriptionKey = "< AZURE-SUBSCRIPTION-KEY >";
+const subscriptionKey = "< AZURE-SUBSCRIPTION-KEY >";
 const faceAttributes = "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
 const detectionModel = "detection_01";
 export const faceApiForUrl = axios.create({
     baseURL: baseURL,
     timeout: 50000,
     headers: {
-        "Ocp-Apim-Subscription-Key": suscriptionKey,
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
         "Content-Type": "application/json"
     },
     params: {
@@ -21,7 +21,7 @@ export const faceApiForUpload = axios.create({
     baseURL: baseURL,
     timeout: 50000,
     headers: {
-        "Ocp-Apim-Subscription-Key": suscriptionKey,
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
         "Content-Type": "application/octet-stream"
     },
     params: {
